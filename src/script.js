ef412a9df434644573b4006bd34fcf23af6e043d
maramota <martacmborges@gmail.com>
2022-11-23 16:59:50 +0000

    updated apiKey

--- src/script.js
+++ src/script.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "c9b2fab8822d7cb6aa87e43122039331";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
